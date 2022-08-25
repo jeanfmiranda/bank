f128eadd9c51e3af00be8ecbb6f6977985659885
Jean Miranda <fjean.miranda@gmail.com>
2022-08-25 17:51:19 -0300

    create the Program, Account and BusinessException classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,49 @@
 package application;
 
+import model.entities.Account;
+import model.exceptions.BusinessException;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            System.out.print("Holder: ");
+            sc.nextLine();
+            String holder = sc.nextLine();
+            System.out.print("Initial balance: ");
+            double initialBalance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            double withdrawLimit = sc.nextDouble();
+
+            Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+
+            System.out.println();
+            System.out.print("Enter amount for withdraw: ");
+            double withdraw = sc.nextDouble();
+            acc.withdraw(withdraw);
+            System.out.println("New balance: " + String.format("%.2f", acc.getBalance()));
+        }
+        catch (BusinessException e) {
+            System.out.println("Withdraw error: " + e.getMessage());
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+        }
+        catch (RuntimeException e) {
+            System.out.println("Unexpected error");
+        }
+        finally {
+            sc.close();
+        }
     }
 }
